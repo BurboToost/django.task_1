49207bf9afa6effe769243d4b2706e608ffcbf5e
BurboToost <aagg3992@gmail.com>
2026-06-20 23:06:15 +0600

    Added todo func

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('dashboard/', views.dashboard),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('todo/', views.home, name='todo'),
+    path('delete/<int:task_id>/' ,  views.delete_task, name= 'delete_task'),
 ]
+
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from.models import Todo
 
+# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        task = request.POST.get('task')
+
+        if task:
+            Todo.objects.create(title=task)
+        return redirect('home')
+
+    tasks= Todo.objects.all().order_by('-id')
+    return render(request, 'index.html',{
+        'tasks':tasks
+    })    
+   
 
 def about(request):
     return render(request, 'about.html')
 
+
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+def delete_task (request, task_id):
+    Todo.objects.filter(id= task_id).delete()
+    return redirect('home')
